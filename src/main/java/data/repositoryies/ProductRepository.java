d99f61785d2ffbb8a685aba8cc890f4d3c677535
metelka342 <segametelka@yandex.ru>
2020-12-03 14:57:10 +0500

    added new services and controllers for entities

--- src/main/java/data/repositoryies/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package data.repositoryies;
-
-import data.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
